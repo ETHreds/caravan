96a44d97761ad657a133bfaff62ab4009b0500c8
ETHreds <jcmaglaw@gmail.com>
2023-03-17 19:35:52 +0300

    Add Bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-from markupsafe import escape
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f'Site  Construction'
+    return render_template('home.html')
 
 
 
