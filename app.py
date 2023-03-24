a7ff5a7b3d3f00121da0d10263c73290cd78ff39
ETHreds <jcmaglaw@gmail.com>
2023-03-25 00:30:21 +0300

    Register and login route

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 
 
